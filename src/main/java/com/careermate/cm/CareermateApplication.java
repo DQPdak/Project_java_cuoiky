41d4680d08e727ac966a3ecbe5c8efa0e61c9fee
Quoc Phong <DQPdak@users.noreply.github.com>
2025-12-08 00:38:27 +0700

    khung sườn cho dự án java

---
+++ src/main/java/com/careermate/cm/CareermateApplication.java
@@ -0,0 +1,12 @@
+package com.careermate.cm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareermateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareermateApplication.class, args);
+    }
+}
