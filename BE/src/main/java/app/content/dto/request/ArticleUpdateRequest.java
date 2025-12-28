fec02c9c871636337feb5c64e615f82fd64c757c
QuocKhanh <khanhlq4979@ut.edu.vn>
2025-12-28 16:25:12 +0700

    Update ArticleUpdateRequest.java

--- BE/src/main/java/app/content/dto/request/ArticleUpdateRequest.java
+++ BE/src/main/java/app/content/dto/request/ArticleUpdateRequest.java
@@ -1,5 +1,23 @@
 package app.content.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class ArticleUpdateRequest {
 
+    @NotBlank
+    @Size(max = 200)
+    private String title;
+
+    @NotBlank
+    private String content;
+
+    @NotBlank
+    @Size(max = 50)
+    private String category;
+
+    private String status; // DRAFT/PUBLISHED
 }
