3dba59c0ecdfc1b51715b6dec654128f3400c360
QuocKhanh <khanhlq4979@ut.edu.vn>
2025-12-30 22:02:51 +0700

    Update ArticleResponse.java

--- BE/src/main/java/app/content/dto/response/ArticleResponse.java
+++ BE/src/main/java/app/content/dto/response/ArticleResponse.java
@@ -1,5 +1,16 @@
 package app.content.dto.response;
 
-public class ArticleResponse {
+import lombok.*;
 
+import java.time.LocalDateTime;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class ArticleResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private String category;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
